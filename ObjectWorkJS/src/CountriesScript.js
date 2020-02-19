e0617318c08687c70f4b7e2d6e95ea4a175815ce
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-02-19 11:09:34 +0300

    Задачка на JS-objects исправления 5

--- ObjectWorkJS/src/CountriesScript.js
+++ ObjectWorkJS/src/CountriesScript.js
@@ -1,48 +1,38 @@
 (function () {
-    function createCountry(name, cities) {
-        return {
-            name: name,
-            cities: cities
-        };
-    }
-
-    function createCity(name, population) {
-        return {
-            name: name,
-            population: population
-        };
-    }
-
-    var countriesArray = [
-        createCountry("Имаджинария", [createCity("Маленький городишка", 2),
-            createCity("Большой городишка", 200), createCity("Город", 100),
-            createCity("Город2", 100), createCity("Город3", 100)]),
-        createCountry("Санктуарий", [createCity("Ромазановск", 900000000)]),
-        createCountry("Новосибирская", [createCity("Помазаников", 900),
-            createCity("Кошатник", 1)]),
-        createCountry("Московская", [createCity("Пошкандыбал", 324534643753243254236243),
-            createCity("Сведловск", 200), createCity("Многолюдовск", 223123124)]),
-        createCountry("Смурфлэнд", [createCity("Смурфятник", 2000000000000000),
-            createCity("Сумрфетник", 123124214), createCity("Смурффффяяяятинаа", 2)]),
-        createCountry("Муркланд", [createCity("Мяу", 900),
-            createCity("МфуМяу", 1000), createCity("Мяяяяу", 2000)])];
+    var countriesArray = [{
+        name: "Имаджинария",
+        cities: [{name: "2134", population: 1}, {name: "2fdf", population: 1}, {name: "2fdf", population: 1},
+            {name: "2fdf68585568", population: 1}]
+    }, {
+        name: "8423758234",
+        cities: [{name: "2134423re", population: 240}, {name: "2fdfwegdg", population: 24},
+            {name: "2fdf324525td3", population: 2424}]
+    }, {
+        name: "8423758dsfdff",
+        cities: [{name: "2134345426e", population: 299}, {name: "2fdfwegdg", population: 25464},
+            {name: "2fdf324525td3", population: 24776824}]
+    }, {
+        name: "832432678567080f",
+        cities: [{name: "213434erwergdfs5426e", population: 299}, {name: "2fdfwegqwewqrt435dg", population: 25008464},
+            {name: "2fdf3-0-525td3", population: 247769989824}]
+    }];
 
     function getMaxCityPopulationCountry(countries) {
         var count = 0;
-        var answer = [];
+        var result = [];
 
         countries.forEach(function (country) {
             var currentCitiesCount = country.cities.length;
 
             if (currentCitiesCount > count) {
                 count = currentCitiesCount;
-                answer = [country.name];
+                result = [country.name];
             } else if (currentCitiesCount === count) {
-                answer.push([country.name]);
+                result.push([country.name]);
             }
         });
 
-        return answer;
+        return result;
     }
 
     function getCityPopulationArray(countries) {
