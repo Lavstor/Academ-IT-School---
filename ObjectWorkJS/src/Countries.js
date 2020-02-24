2be53d5510c40a915f237944da1267b3cf78e42c
Lavstor <netynikneima@gmail.com>
2020-02-24 21:28:07 +0700

    countries - форматирование

--- ObjectWorkJS/src/Countries.js
+++ ObjectWorkJS/src/Countries.js
@@ -1,23 +1,65 @@
 (function () {
-    var countries = [{
-        name: "Имаджинария",
-        cities: [{name: "2134", population: 1}, {name: "2fdf", population: 1}, {name: "2fdf", population: 1},
-            {name: "2fdf68585568", population: 1}]
-    }, {
-        name: "8423758234",
-        cities: [{name: "2134423re", population: 240}, {name: "2fdfwegdg", population: 24},
-            {name: "2fdf324525td3", population: 100}]
-    }, {
-        name: "8423758dsfdff",
-        cities: [{name: "2134345426e", population: 299}, {name: "2fdfwegdg", population: 25464},
-            {name: "2fdf324525td3", population: 24776824}]
-    }, {
-        name: "832432678567080f",
-        cities: [{name: "213434erwergdfs5426e", population: 299}, {name: "2fdfwegqwewqrt435dg", population: 25008464},
-            {name: "2fdf3-0-525td3", population: 247769989824}]
-    }];
+    var countries = [
+        {
+            name: "Имаджинария",
+            cities: [
+                {
+                    name: "2134", population: 1
+                },
+                {
+                    name: "2fdf", population: 1
+                },
+                {
+                    name: "2fdf", population: 1
+                }
+            ]
+        },
+        {
+            name: "8423758234",
+            cities: [
+                {
+                    name: "2134423re", population: 240
+                },
+                {
+                    name: "2fdfwegdg", population: 24
+                },
+                {
+                    name: "2fdf324525td3", population: 100
+                }
+            ]
+        },
+        {
+            name: "8423758dsfdff",
+            cities: [
+                {
+                    name: "2134345426e", population: 299
+                },
+                {
+                    name: "2fdfwegdg", population: 25464
+                },
+                {
+                    name: "2fdf324525td3", population: 24776824
+                }
+            ]
+        },
+        {
+            name: "832432678567080f",
+            cities: [
+                {
+                    name: "213434erwergdfs5426e", population: 299
+                },
+                {
+                    name: "2fdfwegqwewqrt435dg",
+                    population: 25008464
+                },
+                {
+                    name: "2fdf3-0-525td3", population: 247769989824
+                }
+            ]
+        }
+    ];
 
-    function getMaxCityPopulationCountry(countries) {
+    function getMaxCityCountries(countries) {
         var count = 0;
         var result = [];
 
@@ -26,27 +68,27 @@
 
             if (currentCitiesCount > count) {
                 count = currentCitiesCount;
-                result = [country.name];
+                result = [country];
             } else if (currentCitiesCount === count) {
-                result.push([country.name]);
+                result.push([country]);
             }
         });
 
         return result;
     }
 
-    function getCityPopulationArray(countries) {
+    function getCountryPopulationObject(countries) {
         var keyValue = {};
 
         countries.forEach(function (country) {
-            keyValue[country.name] = country.cities.reduce(function (previousValue, sum) {
-                return previousValue + sum.population;
+            keyValue[country.name] = country.cities.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue.population;
             }, 0);
         });
 
         return keyValue;
     }
 
-    console.log("Страна(ы) с максимальным кол-вом городов: ", getMaxCityPopulationCountry(countries).join(", "));
-    console.log("Объект (страна/популяция): ", getCityPopulationArray(countries));
+    console.log("Страна(ы) с максимальным кол-вом городов: ", getMaxCityCountries(countries));
+    console.log("Объект (страна/популяция): ", getCountryPopulationObject(countries));
 })();
