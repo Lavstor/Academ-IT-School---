bb595a0e9572fee58f4f29365cd4ed6f7b6d49ae
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-02-26 14:02:07 +0300

    Задачка на JS-qdasd

--- TodoList/src/Todo.js
+++ TodoList/src/Todo.js
@@ -1,4 +1,20 @@
-$('.close').on('click', function () {
-    console.log("123");
-    console.log($(".list-group-item"));
+$('.newLi').on('click', function () {
+    console.log("add");
+    var newElem = $('<li class="list-group-item list-group-item-action"><a class="redact">asdasd</a>\n' +
+        '                <button type="button" class="close btn-circle" data-dismiss="alert" aria-label="Close">\n' +
+        '                    <span aria-hidden="true">&times;</span>\n' +
+        '                </button>\n' +
+        '            </li><script ></script>');
+    $('.todo-list').append(newElem);
 });
+
+$('.close').on('click', function (e) {
+    console.log("delete");
+    $(e.currentTarget.parentNode).remove();
+});
+
+$('.list-group-item-action').on('click', function (e) {
+    console.log("edit");
+    e.currentTarget.firstChild.nextSibling.innerText = "123";
+});
+
