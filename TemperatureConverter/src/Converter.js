b84e904d5211cc643e4d27c948b83e09e8c91b75
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-02-28 08:22:55 +0300

    Задачка на JS-converter

--- TemperatureConverter/src/Converter.js
+++ TemperatureConverter/src/Converter.js
@@ -1,4 +1,12 @@
 (function () {
+    function getKelvin(value) {
+        return value + 273.15;
+    }
+
+    function getFahrenheit(value) {
+        return value * 9 / 5 + 32;
+    }
+
     var button = document.querySelector(".transfer");
     var inputValue = document.querySelector(".input");
     var fahrenheit = document.querySelector(".fahrenheit");
@@ -8,12 +16,4 @@
         kelvin.innerText = getKelvin(+inputValue.value);
         fahrenheit.innerText = getFahrenheit(+inputValue.value);
     });
-
-    function getKelvin(value) {
-        return value + 273.15;
-    }
-
-    function getFahrenheit(value) {
-        return value * 9 / 5 + 32;
-    }
 })();
