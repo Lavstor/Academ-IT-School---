cbdd7024edb18f094134c325ead9c6f2553c6906
Lavstor <netynikneima@gmail.com>
2020-03-03 01:32:04 +0700

    213214

--- PhoneBookJQuery/src/Table.js
+++ PhoneBookJQuery/src/Table.js
@@ -13,13 +13,23 @@ $(document).ready(function () {
 
         currentId++;
     }).on("click", ".delete-button", function () {
-        console.log("delete");
-        console.log($(this.parentNode.parentNode .currentNumber));
-        $(this.parentNode.parentNode).remove();
+        console.log($(this.closest("tr")));
+
+        $(this.closest("tr")).remove();
         currentId--;
+
+        idReforming();
     });
 
-    function idReforming(toDelete) {
+    function idReforming() {
+        var toChange = $(".main-table .current-number");
+
+        if (toChange.length > 1) {
+            for(var i = currentId - 2; i >= 0; i--){
+                $(toChange[i]).val(i + 1);
+                console.log(toChange[i])
+            }
+        }
     }
 
     var currentId = 1;
