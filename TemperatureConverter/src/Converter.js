770313bc6fe876c150b93bc3b9ccdb904d578ff5
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-03-04 08:05:16 +0300

    Задачка на JS-temp исправления

--- TemperatureConverter/src/Converter.js
+++ TemperatureConverter/src/Converter.js
@@ -1,19 +1,31 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
     function getKelvin(value) {
-        return value + 273.15;
+        return floorNumber(value + 273.15);
     }
 
     function getFahrenheit(value) {
-        return value * 9 / 5 + 32;
+        return floorNumber(value * 9 / 5 + 32);
+    }
+
+    function floorNumber(number) {
+        return Math.floor(number * 100) / 100;
     }
 
     var inputValue = document.querySelector(".input");
     var fahrenheit = document.querySelector(".fahrenheit");
     var kelvin = document.querySelector(".kelvin");
+    var errorMassage = document.querySelector(".error-massage");
 
     var button = document.querySelector(".transfer");
     button.addEventListener("click", function () {
+        if (inputValue.value === "") {
+            errorMassage.style.display = "block";
+            return;
+        }
+
+        errorMassage.style.display = "none";
+
         kelvin.innerText = getKelvin(+inputValue.value);
         fahrenheit.innerText = getFahrenheit(+inputValue.value);
     });
-})();
+});
