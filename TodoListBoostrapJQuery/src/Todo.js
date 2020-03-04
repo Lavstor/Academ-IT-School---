c040231d826baa680044a20a849a631bfa8c453c
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-03-04 13:54:34 +0300

    Задачка на JS-todo jq правки

--- TodoListBoostrapJQuery/src/Todo.js
+++ TodoListBoostrapJQuery/src/Todo.js
@@ -1,47 +1,54 @@
 $(document).ready(function () {
     $('body').on('click', '.newJob', function () {
-        $('.todo-list').append($('' +
-            '<li class="list-group-item list-group-item-action todo">' +
-            '<a class="redact">...</a>' +
+        $('.todo-list').append(newLi("???"));
+    }).on('click', '.close', function () {
+        $(this.parentNode).remove();
+    }).on('click', '.todo', function () {
+            $(this).replaceWith(editableLi(this));
+    });
+
+    function editableLi(node) {
+        var textBefore = node.firstChild.textContent;
+
+        var li = $('<li class="list-group-item list-group-item-action input">' +
+            '<input type="text" placeholder="What u want to do?" class="form-control" aria-label="Default" maxlength= 50>' +
+            '<button type="button" class="cancel btn btn-outline-danger">' +
+            '<span aria-hidden="true">Cancel</span>' +
+            '</button>' +
+            '<button type="button" class="confirm btn btn-outline-success">' +
+            '<span aria-hidden="true">Confirm</span>' +
+            '</button>' +
+            '</li>');
+
+        var input = li[0].children[0];
+        var cancel = li[0].children[1];
+        var confirm = li[0].children[2];
+
+        input.value = textBefore;
+
+        cancel.addEventListener("click", function () {
+            replace (li[0], newLi(textBefore));
+        });
+
+        confirm.addEventListener("click", function () {
+            replace(li[0], newLi(textBefore));
+        });
+
+        return li[0];
+    }
+
+    function newLi(text) {
+         return $('<li class="list-group-item list-group-item-action todo">' +
+            '<a class="redact">'+ text +'</a>' +
             '<button type="button" class="close btn-circle">' +
             '<span aria-hidden="true">&times;' +
             '</span>' +
             '</button>' +
-            '</li>'));
-    }).on('click', '.close', function () {
-        $(this.parentNode).remove();
-    }).on('click', '.todo', function () {
-        if (!isBusy && this.parentNode !== null) {
-            isBusy = true;
-            beforeRedact = this;
-
-            $(this).replaceWith('' +
-                '<li class="list-group-item list-group-item-action input">' +
-                '<input type="text" placeholder="What u want to do?" class="form-control" aria-label="Default" maxlength= 50>' +
-                '<button type="button" class="cancel btn btn-outline-danger">' +
-                '<span aria-hidden="true">Cancel</span>' +
-                '</button>' +
-                '<button type="button" class="confirm btn btn-outline-success">' +
-                '<span aria-hidden="true">Confirm</span>' +
-                '</button>' +
-                '</li>');
-        }
-    }).on('click', '.cancel', function () {
-        replace(this, beforeRedact);
-        isBusy = false;
-    }).on('click', '.confirm', function () {
-        // noinspection JSUnresolvedVariable
-        beforeRedact.firstChild.textContent = this.parentNode.firstChild.value;
-
-        replace(this, beforeRedact);
-        isBusy = false;
-    });
-
-    function replace(convertibleParentNode, convertTo) {
-        $(convertibleParentNode.parentNode).replaceWith(convertTo);
+            '</li>');
     }
 
-    var beforeRedact;
-    var isBusy = false;
+    function replace(convertibleParentNode, convertTo) {
+        convertibleParentNode.parentNode.parentNode.replaceChild(convertTo, convertibleParentNode.parentNode);
+    }
 });
 
