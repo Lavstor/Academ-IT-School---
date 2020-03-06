9cdb246e7ea53eafc58355a74a5d07d9bf02068e
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-03-06 13:33:35 +0300

    Задачка на JS-under

--- Underscore/src/underscore.js
+++ Underscore/src/underscore.js
@@ -5,7 +5,6 @@
             name: "Никита",
             lastName: "Пушкин"
         },
-
         {
             age: 33,
             name: "Никита",
@@ -56,11 +55,11 @@
     var averageAge = _.meanBy(peopleList, "age");
     console.log(averageAge);
 
-    var sortedPeople = _.sortBy(_.filter(peopleList, function (o) {
+    var sortedPeopleList = _.sortBy(_.filter(peopleList, function (o) {
         return o.age <= 30 && o.age >= 20;
     }), "age");
 
-    console.log(sortedPeople);
+    console.log(sortedPeopleList);
 
     var updatedPeopleList = _.each(peopleList, function (currentObj) {
         _.updateWith(currentObj, '[fullName]', _.constant(currentObj.name + ' ' + currentObj.lastName), Object)
