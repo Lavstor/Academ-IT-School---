5031cdba788d8b955dbc9373a314c5788daf96c7
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-03-06 13:26:16 +0300

    Задачка на JS-under

---
+++ Underscore/src/underscore.js
@@ -0,0 +1,70 @@
+(function () {
+    var peopleList = [
+        {
+            age: 16,
+            name: "Никита",
+            lastName: "Пушкин"
+        },
+
+        {
+            age: 33,
+            name: "Никита",
+            lastName: "Мышкин"
+        },
+        {
+            age: 45,
+            name: "Никита",
+            lastName: "Покрышкин"
+        },
+        {
+            age: 57,
+            name: "Никита",
+            lastName: "Абрамович"
+        },
+        {
+            age: 27,
+            name: "Никита",
+            lastName: "Сухоруков"
+        },
+        {
+            age: 26,
+            name: "Никита",
+            lastName: "Шульц"
+        },
+        {
+            age: 23,
+            name: "Никита",
+            lastName: "Адольфович"
+        },
+        {
+            age: 25,
+            name: "Никита",
+            lastName: "Гитлер"
+        },
+        {
+            age: 30,
+            name: "Никита",
+            lastName: "Нетфамильев"
+        },
+        {
+            age: 21,
+            name: "Никита",
+            lastName: "Кошкин"
+        }
+    ];
+
+    var averageAge = _.meanBy(peopleList, "age");
+    console.log(averageAge);
+
+    var sortedPeople = _.sortBy(_.filter(peopleList, function (o) {
+        return o.age <= 30 && o.age >= 20;
+    }), "age");
+
+    console.log(sortedPeople);
+
+    var updatedPeopleList = _.each(peopleList, function (currentObj) {
+        _.updateWith(currentObj, '[fullName]', _.constant(currentObj.name + ' ' + currentObj.lastName), Object)
+    });
+
+    console.log(updatedPeopleList);
+})();
