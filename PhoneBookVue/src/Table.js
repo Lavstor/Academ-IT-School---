8987d39aa2a15868b09e3664243187c0003f713d
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-03-26 13:57:00 +0300

    phone book исправления

--- PhoneBookVue/src/Table.js
+++ PhoneBookVue/src/Table.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     Vue.component('contact-item', {
         template: "<tr class='user-info'>" +
-            "<td><label><input type='checkbox' class='check-box' :checked='contact.checked'></label></td>" +
+            "<td><label><input type='checkbox' class='check-box' " +
+            ":checked='contact.checked' " +
+            "@click='checkBoxPush(contact), $emit(\"check-mass-delete\")'></label></td>" +
             "<td class='current-number can-filter'>{{ index + 1 }}</td>" +
             "<td class='last-name can-filter'>{{ contact.lastName }}</td>" +
             "<td class='first-name can-filter'>{{ contact.firstName }}</td>" +
             "<td class='telephone can-filter'>{{ contact.phone }}</td>" +
             "<td class=\"text-center\"><button type='button' class='delete-button btn btn-danger' @click='$emit(\"delete-contact\")'>X</button></td></tr>",
-        props: ['contact', 'index']
+        props: ['contact', 'index'],
+        methods: {
+            checkBoxPush: function (contact) {
+                contact.checked ? contact.checked = false : contact.checked = true;
+            }
+        }
     });
 
-    new Vue({
+    var vm = new Vue({
         el: "#contacts-list",
         data: {
             newPhone: "",
@@ -22,49 +29,47 @@ document.addEventListener("DOMContentLoaded", function () {
             isEmptyLastName: false,
             isEmptyPhone: false,
             isInvalidPhone: false,
-            massDelete: false
+            massDelete: false,
+            displayError: false,
+            errorMessage: "",
+            filterInput: ""
         },
         methods: {
             newContact: function () {
-                _.each([this.newPhone, this.newFirstName, this.newLastName], function (thisInput) {
-                    if (thisInput.newFirstName === "") {
-                        this.isEmptyFirstName = true;
-
-                        return;
-                    }
-                });
+                this.displayError = false;
+                this.errorMessage = "Ошибка! Пустая(ые) строки!";
+                this.isEmptyFirstName = false;
+                this.isEmptyLastName = false;
+                this.isEmptyPhone = false;
 
                 if (this.newFirstName === "") {
                     this.isEmptyFirstName = true;
-
-                    return;
                 }
-
-                this.isEmptyFirstName = false;
 
                 if (this.newLastName === "") {
                     this.isEmptyLastName = true;
-
-                    return;
                 }
-
-                this.isEmptyLastName = false;
 
                 if (this.newPhone === "") {
                     this.isEmptyPhone = true;
+                }
+
+                if (this.isEmptyPhone || this.isEmptyFirstName || this.isEmptyLastName) {
+                    this.displayError = true;
 
                     return;
                 }
-
-                this.isEmptyPhone = false;
 
                 var newPhone = this.newPhone;
 
                 _.findIndex(this.contacts, function (contact) {
-                    return contact.phone === newPhone;
+                    return contact.phone.toLowerCase() === newPhone.toLowerCase();
                 }) !== -1 ? this.isInvalidPhone = true : this.isInvalidPhone = false;
 
                 if (this.isInvalidPhone) {
+                    this.isEmptyPhone = true;
+                    this.displayError = true;
+                    this.errorMessage = "Ошибка! Такой телефон уже есть!";
                     return;
                 }
 
@@ -77,21 +82,79 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
 
                 this.massDelete = false;
-                this.massDeletePush(true);
-
                 this.newPhone = "";
                 this.newFirstName = "";
                 this.newLastName = "";
             },
             deleteContact: function (index) {
-                this.contacts.splice(index, 1);
+                summonDeleteWindow(function () {
+                    vm.contacts.splice(index, 1);
+                    vm.checkMassDelete();
+                });
             },
-            massDeletePush: function (isChecked) {
-                console.log(isChecked);
+            massDeletePush: function () {
+                this.massDelete ? this.massDelete = false : this.massDelete = true;
+                var isChecked = this.massDelete;
+
                 _.each(this.contacts, function (contact) {
                     contact.checked = isChecked;
                 });
+            },
+            checkMassDelete: function () {
+                if (this.contacts.length === 0) {
+                    this.massDelete = false;
+
+                    return;
+                }
+
+                var isMassDelete = true;
+
+                _.each(this.contacts, function (contact) {
+                    if (contact.checked === false) {
+                        isMassDelete = false;
+                    }
+                });
+
+                this.massDelete = isMassDelete;
+            },
+            deleteAllSelected: function () {
+                // noinspection JSUnusedGlobalSymbols,NonAsciiCharacters
+                summonDeleteWindow(function deleteFunc() {
+                    var deleteIndex = findIndex(vm.contacts);
+
+                    while (deleteIndex >= 0) {
+                        vm.contacts.splice(deleteIndex, 1);
+                        deleteIndex = findIndex(vm.contacts);
+                    }
+
+                    function findIndex(contacts) {
+                        return _.findIndex(contacts, ["checked", true]);
+                    }
+
+                    vm.checkMassDelete();
+                });
+            },
+            confirmFilter: function () {
+                console.log(this.filterInput);
+            },
+            cancelFilter: function () {
+
             }
         }
     });
+
+    function summonDeleteWindow(deleteFunc) {
+        // noinspection NonAsciiCharacters
+        $.confirm({
+            title: "Подтвердите удаление!",
+            content: "Удалить?",
+            buttons: {
+                Удалить: function () {
+                    deleteFunc();
+                },
+                Отмена: function () {
+                }
+            }
+        });
+    }
 });
